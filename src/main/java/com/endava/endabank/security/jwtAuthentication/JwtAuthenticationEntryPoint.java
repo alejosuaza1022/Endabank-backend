cbc64c7278fabfdde6431b08d543355985da1a40
bmgabi95 <98468662+bmgabi95@users.noreply.github.com>
2022-04-08 10:22:28 -0500

    Update JwtAuthenticationEntryPoint.java

--- src/main/java/com/endava/endabank/security/jwtAuthentication/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/endava/endabank/security/jwtAuthentication/JwtAuthenticationEntryPoint.java
@@ -18,7 +18,7 @@ import java.util.Map;
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
         Map<String,String> dataResponse = new HashMap<>();
