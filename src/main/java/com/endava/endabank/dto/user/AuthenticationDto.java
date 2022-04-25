c35a1ea77882e6f09caabe271cbd53a608204d94
J. Var <bramf0@protonmail.com>
2022-04-25 17:16:23 -0500

    remove password pattern and size validation

--- src/main/java/com/endava/endabank/dto/user/AuthenticationDto.java
+++ src/main/java/com/endava/endabank/dto/user/AuthenticationDto.java
@@ -7,8 +7,6 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 @Data
 @Builder
@@ -20,9 +18,6 @@ public class AuthenticationDto {
     private String email;
 
     @NotNull
-    @Size(min = 8, max = 20)
-    @Pattern(regexp = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\W]).{8,20})",
-            message = "1 Capital, 1 Special character, 1 Number and 8 to 20 digits,")
     private String password;
 
     public void setEmail(String email){
