cced8e5f18024fd778b693c56bb332902743cd8a
AnthonyDGallego <Anthony.Gallego@endava.com>
2022-06-06 09:47:58 -0500

    [MEDDV002-1908 Account sumary] Query updated

--- src/main/java/com/endava/endabank/dao/TransactionDao.java
+++ src/main/java/com/endava/endabank/dao/TransactionDao.java
@@ -10,9 +10,9 @@ import org.springframework.data.jpa.repository.Query;
 public interface TransactionDao extends JpaRepository<Transaction, Integer> {
     @Query(value = "select case when bank_account_receiver_id = ?1 then true else false end as wasReceived , amount," +
             " description, create_at createAt, state_type_id stateTypeId, id from transactions" +
-            " where state_type_id  = 1 and bank_account_issuer_id = ?1 or bank_account_receiver_id = ?1 ",
-            countQuery = "select count(*) from transactions where state_type_id  = 1 and bank_account_issuer_id = ?1 " +
-                    "or bank_account_receiver_id = ?1",nativeQuery = true)
+            " where state_type_id  = 1 and (bank_account_issuer_id = ?1 or bank_account_receiver_id = ?1 )",
+            countQuery = "select count(*) from transactions where state_type_id  = 1 and (bank_account_issuer_id = ?1 " +
+                    "or bank_account_receiver_id = ?1 )",nativeQuery = true)
     Page<TransactionDto> getListTransactionsSummary(Integer id, Pageable pageable);
 
 
