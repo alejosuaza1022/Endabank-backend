02722c91cb5964b38bce472a25dc5eaf2d137d9e
J. Var <bramf0@protonmail.com>
2022-06-08 15:21:38 -0500

    add findByUser method to merchant DAO

--- src/main/java/com/endava/endabank/dao/MerchantDao.java
+++ src/main/java/com/endava/endabank/dao/MerchantDao.java
@@ -1,6 +1,7 @@
 package com.endava.endabank.dao;
 
 import com.endava.endabank.model.Merchant;
+import com.endava.endabank.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface MerchantDao extends JpaRepository<Merchant,Integer> {
     Optional<Merchant> findByTaxId(String taxId);
+
+    Optional<Merchant> findByUser(User user);
 }
